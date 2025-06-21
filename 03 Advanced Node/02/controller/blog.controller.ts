25703969c656fdc063e7c5c7ad811450609509a2
Michal Radomski <michalra@op.pl>
2025-06-22 00:10:50 +0200

    Advanced Node course - commit 15

--- 03 Advanced Node/02/controller/blog.controller.ts
+++ 03 Advanced Node/02/controller/blog.controller.ts
@@ -1,32 +1,46 @@
 import { Request, RequestHandler, Response } from "express";
 
 import Blog, { BlogI } from "../models/blog.model";
-import createBlogSchema from "../validations/blog.validation";
+// import createBlogSchema from "../validations/blog.validation";
+import { catchAsync } from "../utils/catchAsync";
 
-export const createBlog: RequestHandler = async (req: Request, res: Response): Promise<void> => {
-  try {
-    // Validate the request body
-    const blog = (await createBlogSchema.body.validateAsync(req.body)) as BlogI;
+//* V1
+// export const createBlog: RequestHandler = async (req: Request, res: Response): Promise<void> => {
+//   try {
+//     // Validate the request body
+//     const blog = (await createBlogSchema.body.validateAsync(req.body)) as BlogI;
 
-    // Create the blog with the validated data
-    await Blog.create(blog);
+//     // Create the blog with the validated data
+//     await Blog.create(blog);
 
-    res.send({ success: true, message: "Blog created successfully" });
-  } catch (error) {
-    if (error instanceof Error) {
-      res.send({ error: true, message: error.message });
-    }
-  }
-};
+//     res.send({ success: true, message: "Blog created successfully" });
+//   } catch (error) {
+//     if (error instanceof Error) {
+//       res.send({ error: true, message: error.message });
+//     }
+//   }
+// };
 
-export const getBlogs: RequestHandler = async (req: Request, res: Response): Promise<void> => {
+// export const getBlogs: RequestHandler = async (req: Request, res: Response): Promise<void> => {
+//   console.log("req.ip:", req.ip);
+//   try {
+//     const blogs = await Blog.find({});
+//     res.json(blogs);
+//   } catch (error) {
+//     if (error instanceof Error) {
+//       res.send({ error: true, message: error.message });
+//     }
+//   }
+// };
+
+//* V2
+export const createBlog: RequestHandler = catchAsync(async (req: Request, res: Response): Promise<void> => {
+  await Blog.create(req.body);
+  res.send({ success: true, message: "Blog created successfully" });
+});
+
+export const getBlogs: RequestHandler = catchAsync(async (req: Request, res: Response): Promise<void> => {
   console.log("req.ip:", req.ip);
-  try {
-    const blogs = await Blog.find({});
-    res.json(blogs);
-  } catch (error) {
-    if (error instanceof Error) {
-      res.send({ error: true, message: error.message });
-    }
-  }
-};
+  const blogs = (await Blog.find({})) as BlogI[];
+  res.json(blogs);
+});
