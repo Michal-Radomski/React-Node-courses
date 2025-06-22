323f95f156c4830a4c5ccda3dfdc5213fe8cedde
Michal Radomski <michalra@op.pl>
2025-06-22 23:25:05 +0200

    Advanced Node course - commit 26

---
+++ 03 Advanced Node/03/index.ts
@@ -0,0 +1,20 @@
+import { program } from "commander";
+import inquirer from "inquirer";
+
+program.version("1.0.0").description("A simple CLI program.");
+
+program.command("start").action(async () => {
+  const prompt = inquirer.createPromptModule();
+  const answer = await prompt([
+    { name: "username", message: "What is your name?", type: "input" },
+    {
+      name: "experience",
+      message: "How many years of experience do you have?",
+      type: "list",
+      choices: ["1-3", "3-5", "5-10", ">10"],
+    },
+  ]);
+  console.log(`Hi ${answer.username} you have ${answer.experience} years of experience.`);
+});
+
+program.parse(process.argv);
