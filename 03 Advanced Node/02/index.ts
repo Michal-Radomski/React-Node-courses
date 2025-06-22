853ccb2f3c98ad15a69017ffc1990ca3baa8be11
Michal Radomski <michalra@op.pl>
2025-06-22 13:41:59 +0200

    Advanced Node course - commit 20

--- 03 Advanced Node/02/index.ts
+++ 03 Advanced Node/02/index.ts
@@ -1,69 +1,8 @@
-import path from "path";
 import http from "http";
-import express, { Express, Request, Response } from "express";
 import mongoose from "mongoose";
-import cors from "cors";
-import bodyParser from "body-parser";
-import helmet from "helmet";
-import morgan from "morgan";
-import compression from "compression";
-import httpStatus from "http-status";
-// import * as dotenv from "dotenv";
-// dotenv.config();
 
 import config from "./config/config";
-import { errorConverter, errorHandler } from "./middlewares/error";
-import ApiError from "./utils/ApiError";
-//* Import routes
-import blogRouter from "./routes/blog.route";
-
-const app: Express = express();
-
-const corsOptions = {
-  origin: true,
-  methods: ["POST", "GET", "OPTIONS", "PUT", "DELETE"],
-  preflightContinue: false,
-  optionsSuccessStatus: 200,
-  credentials: true,
-  allowedHeaders: ["Content-Type", "Authorization", "Accept"], // Specify
-};
-
-//* Middleware
-app.use(cors(corsOptions));
-app.use(bodyParser.json());
-app.use(bodyParser.urlencoded({ extended: true }));
-app.use(morgan("combined"));
-app.use(
-  helmet({
-    contentSecurityPolicy: false,
-    crossOriginResourcePolicy: false,
-    crossOriginEmbedderPolicy: false,
-    crossOriginOpenerPolicy: false,
-  })
-);
-// Compress all responses
-app.use(compression({ level: 6 }));
-
-//* Route Middleware
-app.use(blogRouter);
-
-//* Favicon
-app.get("/favicon.ico", (_req: Request, res: Response) => {
-  res.sendFile(path.join(__dirname + "/favicon.svg"));
-});
-//* Test route
-app.get("/", (req: Request, res: Response) => {
-  console.log("req.ip:", req.ip);
-  res.send("<h1 style='color:blue;text-align:center'>API is running</h1>");
-});
-
-//* Error Handler
-app.use((_req, _res, next) => {
-  next(new ApiError(httpStatus.NOT_FOUND, "Not found"));
-});
-
-app.use(errorConverter); // Add the error converter before the error handler
-app.use(errorHandler); // Add the error handler
+import app from "./server/server";
 
 //* Mongo DB
 mongoose.set("strictQuery", true);
@@ -105,3 +44,9 @@ const unExpectedErrorHandler = (error: Error): void => {
 
 process.on("uncaughtException", unExpectedErrorHandler);
 process.on("unhandledRejection", unExpectedErrorHandler);
+process.on("SIGTERM", () => {
+  console.log("SIGTERM received");
+  if (httpServer) {
+    httpServer.close();
+  }
+});
