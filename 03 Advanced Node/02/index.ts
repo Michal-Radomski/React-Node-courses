aa332ace5e6ab42426fb04db5a91bd6b5f0fc7e0
Michal Radomski <michalra@op.pl>
2025-06-22 13:18:11 +0200

    Advanced Node course - commit 18

--- 03 Advanced Node/02/index.ts
+++ 03 Advanced Node/02/index.ts
@@ -7,11 +7,13 @@ import bodyParser from "body-parser";
 import helmet from "helmet";
 import morgan from "morgan";
 import compression from "compression";
+import httpStatus from "http-status";
 // import * as dotenv from "dotenv";
 // dotenv.config();
 
 import config from "./config/config";
 import { errorConverter, errorHandler } from "./middlewares/error";
+import ApiError from "./utils/ApiError";
 //* Import routes
 import blogRouter from "./routes/blog.route";
 
@@ -45,7 +47,21 @@ app.use(compression({ level: 6 }));
 //* Route Middleware
 app.use(blogRouter);
 
+//* Favicon
+app.get("/favicon.ico", (_req: Request, res: Response) => {
+  res.sendFile(path.join(__dirname + "/favicon.svg"));
+});
+//* Test route
+app.get("/", (req: Request, res: Response) => {
+  console.log("req.ip:", req.ip);
+  res.send("<h1 style='color:blue;text-align:center'>API is running</h1>");
+});
+
 //* Error Handler
+app.use((_req, _res, next) => {
+  next(new ApiError(httpStatus.NOT_FOUND, "Not found"));
+});
+
 app.use(errorConverter); // Add the error converter before the error handler
 app.use(errorHandler); // Add the error handler
 
@@ -59,16 +75,6 @@ mongoose
   })
   .catch((error: string) => console.log("Mongo DB Error => ", error));
 
-//* Favicon
-app.get("/favicon.ico", (_req: Request, res: Response) => {
-  res.sendFile(path.join(__dirname + "/favicon.svg"));
-});
-//* Test route
-app.get("/", (req: Request, res: Response) => {
-  console.log("req.ip:", req.ip);
-  res.send("<h1 style='color:blue;text-align:center'>API is running</h1>");
-});
-
 //* Port
 // const portHTTP = (process.env.PORT || 5000) as number;
 const portHTTP = (config.port || 5000) as number;
@@ -80,3 +86,22 @@ httpServer.listen({ port: portHTTP, host: "127.0.0.1" }, () => {
   // For testing only
   console.log("Current Time:", new Date().toLocaleTimeString());
 });
+
+const exitHandler = (): void => {
+  if (httpServer) {
+    httpServer.close(() => {
+      console.log("Server closed");
+      process.exit(1);
+    });
+  } else {
+    process.exit(1);
+  }
+};
+
+const unExpectedErrorHandler = (error: Error): void => {
+  console.log(error);
+  exitHandler();
+};
+
+process.on("uncaughtException", unExpectedErrorHandler);
+process.on("unhandledRejection", unExpectedErrorHandler);
