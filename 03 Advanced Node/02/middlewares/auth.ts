83b400a0d5b5d6e03adda657a7cc30bbdb4e18cd
Michal Radomski <michalra@op.pl>
2025-06-26 01:08:46 +0200

    Advanced Node course - commit 36

---
+++ 03 Advanced Node/02/middlewares/auth.ts
@@ -0,0 +1,26 @@
+import { NextFunction, Request, RequestHandler, Response } from "express";
+import passport from "passport";
+import httpStatus from "http-status";
+
+import ApiError from "../utils/ApiError";
+
+const verifyCallBack =
+  (req: Request, resolve: Function, reject: Function) =>
+  async (err: Error, user: Express.User, info: any): Promise<void> => {
+    if (err || info || !user) {
+      return reject(new ApiError(httpStatus.UNAUTHORIZED, "please authenticate"));
+    }
+
+    req.user = user;
+    resolve();
+  };
+
+const auth: RequestHandler = async (req: Request, res: Response, next: NextFunction): Promise<void> => {
+  return new Promise((resolve, reject) => {
+    passport.authenticate("jwt", { session: false }, verifyCallBack(req, resolve, reject))(req, res, next);
+  })
+    .then(() => next())
+    .catch((error) => next(error));
+};
+
+export default auth;
--- 03 Advanced Node/02/routes/blog.route.ts
+++ 03 Advanced Node/02/routes/blog.route.ts
@@ -3,11 +3,12 @@ import express, { Router } from "express";
 import { getBlogs, createBlog } from "../controller/blog.controller";
 import validate from "../middlewares/validate";
 import createBlogSchema from "../validations/blog.validation";
+import auth from "../middlewares/auth";
 
 const blogRouter: Router = express.Router();
 
-blogRouter.get("/blogs", getBlogs);
+blogRouter.get("/blogs", auth, getBlogs);
 // blogRouter.post("/blog", createBlog);
-blogRouter.post("/blog", validate(createBlogSchema), createBlog);
+blogRouter.post("/blog", auth, validate(createBlogSchema), createBlog);
 
 export default blogRouter;
