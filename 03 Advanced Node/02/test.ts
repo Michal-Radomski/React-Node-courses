6b89bf02e1295dd5fcfd6ba741407ed172a9ae2b
Michal Radomski <michalra@op.pl>
2025-06-27 00:53:43 +0200

    Advanced Node course - commit 43

---
+++ 03 Advanced Node/02/test.ts
@@ -0,0 +1,19 @@
+//* Regular expression denial of service example
+
+import safe from "safe-regex";
+
+const regex: RegExp = /^([a-z]+.)+[A-Za-z]+$/;
+
+console.log("safe(regex):", safe(regex));
+
+let str = "";
+
+for (let i = 0; i < 100; i++) {
+  str += "a";
+}
+
+str = str + "7";
+console.log("str:", str);
+
+console.log(str.match(regex));
+console.log("something");
