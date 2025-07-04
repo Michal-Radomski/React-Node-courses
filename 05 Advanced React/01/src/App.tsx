f76b983b98cf8d23b7c9fb2c174d34fe79015a8c
Michal Radomski <michalra@op.pl>
2025-07-04 21:12:52 +0200

    Advanced React: Design System, Design Patterns course - commit 02

--- 05 Advanced React/01/src/App.tsx
+++ 05 Advanced React/01/src/App.tsx
@@ -1,9 +1,25 @@
 import React from "react";
 
 import "./App.scss";
+import SplitScreen from "./components/SplitScreen";
+
+const LeftSideComp = ({ title }: { title: string }): JSX.Element => {
+  return <h2 style={{ backgroundColor: "crimson" }}>{title}</h2>;
+};
+
+const RightSideComp = ({ title }: { title: string }): JSX.Element => {
+  return <h2 style={{ backgroundColor: "burlywood" }}>{title}</h2>;
+};
 
 const App = (): JSX.Element => {
-  return <React.Fragment>App</React.Fragment>;
+  return (
+    <React.Fragment>
+      <SplitScreen leftWidth={1} rightWidth={3}>
+        <LeftSideComp title={"Right"} />
+        <RightSideComp title={"Left"} />
+      </SplitScreen>
+    </React.Fragment>
+  );
 };
 
 export default App;
---
+++ 05 Advanced React/01/src/components/SplitScreen.tsx
@@ -0,0 +1,29 @@
+import styled from "styled-components";
+
+const Container = styled.div`
+  display: flex;
+`;
+
+const Panel = styled.div<{ $flex: number }>`
+  flex: ${(p) => p.$flex};
+`;
+
+export const SplitScreen = ({
+  children,
+  leftWidth = 1,
+  rightWidth = 1,
+}: {
+  children: React.ReactNode[];
+  leftWidth: number;
+  rightWidth: number;
+}): JSX.Element => {
+  const [left, right] = children;
+  return (
+    <Container>
+      <Panel $flex={leftWidth}>{left}</Panel>
+      <Panel $flex={rightWidth}>{right}</Panel>
+    </Container>
+  );
+};
+
+export default SplitScreen;
