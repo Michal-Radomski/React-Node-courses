9227ab306249b3f57ce8a3677d0644e80f230a88
Michal Radomski <michalra@op.pl>
2025-07-06 23:42:27 +0200

    Advanced React: Design System, Design Patterns course - commit 22

--- 05 Advanced React/04/src/App.tsx
+++ 05 Advanced React/04/src/App.tsx
@@ -4,12 +4,17 @@ import "./App.scss";
 import { checkProps } from "./components/check-props";
 import { UserInfo } from "./components/user-info";
 import { users } from "../data";
+import { includeUser } from "./components/include-user";
 
 const UserInfoWrapper = checkProps(UserInfo);
+const UserInfoWithUser = includeUser(UserInfo, "2");
 
 const App = (): JSX.Element => {
   return (
     <React.Fragment>
+      <UserInfoWithUser />
+      <br />
+
       <UserInfoWrapper user={users[0]} />
     </React.Fragment>
   );
---
+++ 05 Advanced React/04/src/components/include-user.tsx
@@ -0,0 +1,21 @@
+import React from "react";
+import axios from "axios";
+
+type UserComponentProps = {
+  user: User;
+};
+
+export const includeUser = (Component: React.ComponentType<UserComponentProps>, userId: string) => {
+  return (): JSX.Element => {
+    const [user, setUser] = React.useState<User | null>(null);
+
+    React.useEffect(() => {
+      (async (): Promise<void> => {
+        const response = await axios.get(`/api/users/${userId}`);
+        setUser(response.data);
+      })();
+    });
+
+    return <React.Fragment>{user ? <Component user={user} /> : null}</React.Fragment>;
+  };
+};
