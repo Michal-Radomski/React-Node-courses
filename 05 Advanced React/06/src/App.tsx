0242a78fcb72b2ec92165828cedbf92ace0883d4
Michal Radomski <michalra@op.pl>
2025-07-08 21:55:19 +0200

    Advanced React: Design System, Design Patterns course - commit 34

---
+++ 05 Advanced React/06/src/App.tsx
@@ -0,0 +1,44 @@
+import React from "react";
+import { createPortal } from "react-dom";
+
+import "./App.scss";
+
+const App = (): JSX.Element => {
+  const [show, setShow] = React.useState<boolean>(false);
+
+  return (
+    <React.Fragment>
+      <div style={{ position: "absolute", marginTop: "200px" }}>
+        <h1>Other Content</h1>
+        <button onClick={() => setShow(true)}>Show Message</button>
+        <Alert show={show} onClose={() => setShow(false)}>
+          A sample message to show.
+          <br />
+          Click it to close.
+        </Alert>
+      </div>
+    </React.Fragment>
+  );
+};
+
+const Alert = ({
+  children,
+  onClose,
+  show,
+}: {
+  children: React.ReactNode;
+  onClose: () => void;
+  show: boolean;
+}): JSX.Element => {
+  // eslint-disable-next-line @typescript-eslint/no-explicit-any
+  if (!show) return null as any;
+
+  return createPortal(
+    <div className="alert" onClick={onClose}>
+      {children}
+    </div>,
+    document.querySelector("#alert-holder") as HTMLDivElement
+  );
+};
+
+export default App;
