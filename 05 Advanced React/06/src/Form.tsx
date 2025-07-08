4e7c3f7f467358d8a1aae54fc2b65c00206504a2
Michal Radomski <michalra@op.pl>
2025-07-08 23:37:22 +0200

    Advanced React: Design System, Design Patterns course - commit 41

--- 05 Advanced React/06/src/Form.tsx
+++ 05 Advanced React/06/src/Form.tsx
@@ -27,7 +27,7 @@ const Form = (): JSX.Element => {
         It is a long established fact that a reader will be distracted by the readable content of a page when looking at its
         layout.
       </p>
-      <Form />
+      <Input />
     </React.Fragment>
   );
 };
