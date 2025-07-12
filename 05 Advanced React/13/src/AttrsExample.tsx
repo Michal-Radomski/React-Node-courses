7d91ff8cacd45b7c8107f2eb36886c1ddfafbcae
Michal Radomski <michalra@op.pl>
2025-07-12 23:19:37 +0200

    Advanced React: Design System, Design Patterns course - commit 81

--- 05 Advanced React/13/src/AttrsExample.tsx
+++ 05 Advanced React/13/src/AttrsExample.tsx
@@ -1,7 +1,7 @@
 import React from "react";
 import styled from "styled-components";
 
-//* Is it ok?
+//* Is it ok? - Perplexity
 interface AdditionalProps {
   $size?: string;
   $margin?: string;
@@ -32,12 +32,38 @@ const PasswordInput = styled.input.attrs({
   type: "password",
 })``;
 
+//* Grok
+// Define a type for the component's props
+interface InputProps {
+  testId?: string; // Optional prop for dynamic data-test attribute
+}
+
+// Create a styled input with attrs
+const StyledInput = styled.input.attrs<InputProps>(({ testId }) => ({
+  type: "text", // Static attribute
+  placeholder: "Enter your name", // Static attribute
+  "data-test": testId || "input-default", // Dynamic attribute based on prop
+}))`
+  padding: 8px;
+  border: 2px solid #007bff;
+  border-radius: 4px;
+  font-size: 16px;
+
+  &:focus {
+    outline: none;
+    border-color: #0056b3;
+  }
+`;
+
 const AttrsExample = (): JSX.Element => {
   return (
     <React.Fragment>
       <Input placeholder="Default size" />
       <Input placeholder="Large size" $size="2em" />
       <PasswordInput placeholder="password" />
+      <StyledInput testId="name-input" />
+      {/* Will use default data-test="input-default" */}
+      <StyledInput />
     </React.Fragment>
   );
 };
