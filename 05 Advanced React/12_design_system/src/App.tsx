9995eaeeed70059a4d93844a4be113968f3fbac5
Michal Radomski <michalra@op.pl>
2025-07-12 17:27:27 +0200

    Advanced React: Design System, Design Patterns course - commit 73

--- 05 Advanced React/12_design_system/src/App.tsx
+++ 05 Advanced React/12_design_system/src/App.tsx
@@ -1,9 +1,61 @@
 import React from "react";
+import { ThemeProvider } from "styled-components";
+
+import { PrimaryButton, SecondaryButton } from "./components/Buttons";
+import { GlobalStyles, darkTheme, primaryTheme } from "./style";
+import { RegisterModal } from "./components/RegisterModal";
 
 import "./App.scss";
 
 const App = (): JSX.Element => {
-  return <React.Fragment>App</React.Fragment>;
+  const [useDarkTheme, setDarkTheme] = React.useState<boolean>(false);
+  return (
+    <React.Fragment>
+      <button
+        style={{
+          margin: "0 18px 26px",
+          padding: "10px",
+          background: "none",
+          cursor: "pointer",
+          border: "3px solid #FFFF",
+        }}
+        onClick={() => setDarkTheme(false)}
+      >
+        Light
+      </button>
+      <button
+        style={{
+          margin: "0 18px 26px",
+          padding: "10px",
+          background: "none",
+          cursor: "pointer",
+          border: "3px solid #FFFF",
+        }}
+        onClick={() => setDarkTheme(true)}
+      >
+        Dark
+      </button>
+      <ThemeProvider theme={useDarkTheme ? darkTheme : primaryTheme}>
+        <div
+          style={{
+            background: useDarkTheme ? primaryTheme.primaryColor : darkTheme.primaryColor,
+            position: "absolute",
+            left: "0",
+            width: "100vw",
+            height: "90vh",
+            display: "flex",
+            alignItems: "center",
+            justifyContent: "space-around",
+          }}
+        >
+          <PrimaryButton>Click me!</PrimaryButton>
+          <SecondaryButton>Click me too!</SecondaryButton>
+          <GlobalStyles />
+        </div>
+        <RegisterModal />
+      </ThemeProvider>
+    </React.Fragment>
+  );
 };
 
 export default App;
---
+++ 05 Advanced React/12_design_system/src/components/RegisterModal.tsx
@@ -0,0 +1,21 @@
+import React from "react";
+
+import { Images, CloseIcon } from "../assets";
+import { PrimaryButton } from "./Buttons";
+import { CloseIconContainer, RegisterContainer, RegisterHeader, RegisterHelper } from "./Register";
+
+export const RegisterModal = (): JSX.Element => {
+  return (
+    <React.Fragment>
+      <RegisterContainer>
+        <img style={{ maxWidth: "350px" }} src={Images.register} alt="Register" />
+        <RegisterHeader>Register</RegisterHeader>
+        <RegisterHelper>Register and unlock all the features!</RegisterHelper>
+        <PrimaryButton onClick={() => alert("Registered!")}>Register</PrimaryButton>
+        <CloseIconContainer onClick={() => alert("Closed")}>
+          <CloseIcon />
+        </CloseIconContainer>
+      </RegisterContainer>
+    </React.Fragment>
+  );
+};
