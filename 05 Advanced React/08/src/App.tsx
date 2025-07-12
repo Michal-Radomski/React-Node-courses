b292fff4b9760af575c6534e7da9b742f59b8654
Michal Radomski <michalra@op.pl>
2025-07-12 15:03:39 +0200

    Advanced React: Design System, Design Patterns course - commit 70

--- 05 Advanced React/08/src/App.tsx
+++ 05 Advanced React/08/src/App.tsx
@@ -2,11 +2,17 @@ import React from "react";
 
 import "./App.scss";
 import SearchMeal from "./components/search-meals";
+import TrackMouse from "./throttle/track-mouse";
 // import Users from "./components/users";
 
 const App = (): JSX.Element => {
   return (
     <React.Fragment>
+      {/* //* Debouncing is a technique that delays the execution until a specific amount of time */}
+
+      {/* //* Throttling: only once for a specific time */}
+      <TrackMouse />
+
       <SearchMeal />
       {/* <Users /> */}
     </React.Fragment>
