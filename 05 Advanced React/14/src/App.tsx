c0921335ba50976a46204df7b40da426f83ceca4
Michal Radomski <michalra@op.pl>
2025-07-15 00:21:16 +0200

    Advanced React: Design System, Design Patterns course - commit 113

--- 05 Advanced React/14/src/App.tsx
+++ 05 Advanced React/14/src/App.tsx
@@ -33,7 +33,25 @@ export const fetchBooks = async (count: number): Promise<BookI[]> => {
 //   return [url, setUrl] as const;
 // };
 
+const useLocalStorage = <T,>(identifier: string) => {
+  const set = (key: string, value: T) => {
+    window.localStorage.setItem(key + identifier, JSON.stringify(value));
+  };
+
+  const get = (key: string): T | null => {
+    return JSON.parse(window.localStorage.getItem(key + identifier) || "null");
+  };
+  return { set, get };
+};
+
 const App = (): JSX.Element => {
+  const client = useLocalStorage<{ level: string }>("client");
+
+  client.set("level", { level: "student" });
+
+  const clientLevel = client.get("level");
+  console.log({ clientLevel });
+
   // const state = useUser("https://jsonplaceholder.typicode.com/users/1");
   // console.log("state:", state);
 
